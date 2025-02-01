add389aaab2aff4465193530b00c26677d9d5e24
Rupesh <rupeshahuja28@gmail.com>
2025-02-01 13:49:16 +0000

    Changed Description

--- my-app/src/components/Nav/Sidebar.jsx
+++ my-app/src/components/Nav/Sidebar.jsx
@@ -10,7 +10,7 @@ export default function Sidebar({ sidebarOpen, toggleSidebar }) {
     <Wrapper className="animate darkBg" sidebarOpen={sidebarOpen}>
       <SidebarHeader className="flexSpaceCenter">
         <div className="flexNullCenter">
-          {/* <LightLogoImg /> */}
+          <LightLogoImg />
           {/* <h1 className="whiteColor font20" style={{ marginLeft: "15px" }}>
             RoboLearn
           </h1> */}
